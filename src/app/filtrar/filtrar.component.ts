4f111a9cab2a754586dfc00244db687b2cd83da6
Ezequiel <exzequielfalcon@gmail.com>
2020-11-26 20:40:41 -0300

    Fixed filters mobile and desktop

--- src/app/filtrar/filtrar.component.ts
+++ src/app/filtrar/filtrar.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit,EventEmitter } from '@angular/core';
 import { MatOption } from '@angular/material/core';
 import { FormControl } from '@angular/forms';
+import {
+  fadeInOnEnterAnimation,
+  fadeOutOnLeaveAnimation,
+  slideInDownAnimation,
+  slideInDownOnEnterAnimation, slideInLeftOnEnterAnimation,
+  slideOutDownOnLeaveAnimation, slideOutLeftOnLeaveAnimation, slideOutUpOnLeaveAnimation
+} from 'angular-animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-filtrar',
   templateUrl: './filtrar.component.html',
-  styleUrls: ['./filtrar.component.scss']
+  styleUrls: ['./filtrar.component.scss'],
+  animations: [slideInDownOnEnterAnimation(), slideOutUpOnLeaveAnimation({duration: 500}), trigger('slideOut',[
+    transition(':leave', [
+      animate('200ms ease-in', style({ opacity: '0' }))
+    ])
+  ])]
 })
 export class FiltrarComponent implements OnInit {
   show: boolean = false;
